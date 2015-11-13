dfc3a77653ed3c3a44b1a8ba7e80ae601f1bf754
Juliana <mjbedoya@ufl.edu>
2015-11-13 13:45:37 -0500

    Calculation of the mean mass of the extinct and extant species

--- Mammals_Analysis.R
+++ Mammals_Analysis.R
@@ -1,7 +1,8 @@
 ## R Assigment 8 --All together
 
-## 1. --Size-biased extinction--
+library(dplyr)
 
+# Data from ()
 mammal_sizes<-read.csv("MOMv3.3.txt", sep = "\t", head=FALSE, stringsAsFactors = FALSE, na.strings = "-999")
 
 colnames(mammal_sizes) <- c("continent", "status", "order", 
@@ -9,3 +10,22 @@ colnames(mammal_sizes) <- c("continent", "status", "order",
                             "reference")
 head(mammal_sizes)
 summary(mammal_sizes) 
+names(mammal_sizes)
+getClassName(mammal_sizes)
+
+
+## 1. --Size-biased extinction--
+
+## Calculation of the mean mass of the extinct species and the mean mass of the extant species:
+
+unique(mammal_sizes$status)   # used to get the different classes of the status variable
+
+select(mammal_sizes, status, combined_mass)
+filter_status<-filter(mammal_sizes, status =="extant"| status =="extinct")
+unique(filter_status$status)  # Review the selected columns
+
+by_status<-group_by(filter_status,status); head(by_status_extant)
+
+mean_mass <- summarize(by_status, avg_mass = mean(combined_mass, na.rm=TRUE)) ; mean_mass # remove NA inside combined_mass
+
+
