cabeee56754f57b1b6670938810c1e3e17ff8580
Juliana <mjbedoya@ufl.edu>
2015-11-16 22:19:33 -0500

    Plot of the log_mass of the extanct and extinct species and the graph is added

--- Mammals_Analysis.R
+++ Mammals_Analysis.R
@@ -41,3 +41,24 @@ extant_extinct_mass <- continent_by_status %>%
 
 write.table(extant_extinct_mass, "continent_mass_differences.csv") # Use sep = ",",  if you want an organized table in columns when opening in excel
 
+# 1d. Plot: Formatting the data to be plotted
+
+filter_status<-filter(mammal_sizes, status =="extant" | status =="extinct")  ; head(filter_status) ; unique(filter_status$status)  # Review the selected columns
+by_status<-group_by(filter_status,status); head(by_status)
+status_logmass_continent <- select(by_status, continent, log_mass, status) ; status_logmass_continent
+filter2<-filter(status_logmass_continent, log_mass != "NA")  # to remove the "Af"
+# table(filter2$continent,filter2$status)  # to check if I removed the NAs in Af
+extinct_extanct_continent<-filter(filter2, continent != "EA" & continent != "Oceanic")
+# table(extinct_extanct_continent$continent,extinct_extanct_continent$status)   # Used to check the data
+
+# Final Plot
+
+png("extinct_extanct_graph.png")
+qplot(log_mass, data=extinct_extanct_continent, geom = "histogram", binwidth = 1/6)+
+  xlim(c(0,8))+
+  facet_grid(continent~status)+
+  xlab("Log (Mass)")+
+  ylab("No. Species")+
+  ggtitle("Number of extant and extinct species per continent")
+dev.off()
+
